02d3d2ccf5e9da090f99ab55f6bcbf82d4e46071
Leo Thesen <leothesen@gmail.com>
2023-07-13 07:52:30 +0200

    feat(Value): :zap: upserted value row for user
    to ensure that only one row exists per user

--- src/server/api/routers/value.ts
+++ src/server/api/routers/value.ts
@@ -6,8 +6,14 @@ export const valueRouter = createTRPCRouter({
   create: protectedProcedure
     .input(z.object({ values: z.array(z.string()) }))
     .mutation(async ({ ctx, input }) => {
-      return ctx.prisma.values.create({
-        data: {
+      return ctx.prisma.values.upsert({
+        where: {
+          userId: ctx.auth.userId,
+        },
+        update: {
+          values: input.values,
+        },
+        create: {
           values: input.values,
           userId: ctx.auth.userId,
         },
