0b942e10ab5e37fd6afd7bbb05da1052d23ce552
Leo Thesen <leothesen@gmail.com>
2023-07-27 23:18:09 +0200

    fix(Wheel): :bug: the wheel was failing to render server side
    and added some cute descriptions for the wheel at various places in the journey

--- src/components/radarChart/index.tsx
+++ src/components/radarChart/index.tsx
@@ -4,24 +4,24 @@ import RadarChart from "react-svg-radar-chart";
 const mockData = [
   {
     data: {
-      health: 0.9,
-      sleep: 0.2,
-      work: 0.2,
-      relationships: 0.4,
-      nature: 0.5,
-      exercise: 0.2,
+      something: 0,
+      has: 0.1,
+      gone: 0.2,
+      wrong: 0.3,
+      whoops: 0.4,
+      sorry: 0.5,
     },
     meta: { color: "blue" },
     date: new Date(),
   },
   {
     data: {
-      health: 0.9,
-      sleep: 0.2,
-      work: 0.2,
-      relationships: 0.4,
-      nature: 0.5,
-      exercise: 0.2,
+      something: 0.9,
+      has: 0.6,
+      gone: 0.4,
+      wrong: 0.2,
+      whoops: 0.1,
+      sorry: 0.0,
     },
     meta: { color: "red" },
     date: new Date(),
@@ -29,12 +29,12 @@ const mockData = [
 ];
 
 const mockValues = {
-  health: "Health",
-  sleep: "Sleep",
-  work: "Work",
-  relationships: "Relationships",
-  nature: "Nature",
-  exercise: "Exercise",
+  something: "something",
+  has: "has",
+  gone: "gone",
+  wrong: "wrong",
+  whoops: "whoops",
+  sorry: "sorry",
 };
 
 export interface ChartData {
@@ -52,32 +52,36 @@ interface Props {
   size?: number;
 }
 
+const getInitialScreenWidth = () => {
+  if (typeof window !== 'undefined') {
+    return window.innerWidth;
+  } else {
+    return 1024; // Default value when window is not available (server-side)
+  }
+};
+
 export const RadarGraph: React.FC<Props> = ({
   data = mockData,
   values = mockValues,
-  size = 700,
+  size = 600,
 }) => {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenWidth, setScreenWidth] = useState(getInitialScreenWidth);
 
   useEffect(() => {
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
+
+
 
   // Calculate the size based on breakpoint
   if (screenWidth < 480) {
     size = 300;
   } else if (screenWidth < 768) {
-    size = 500;
+    size = 480;
   } else if (screenWidth < 1024) {
-    size = 700;
+    size = 600;
   } // Add more breakpoints as needed
 
   return <RadarChart captions={values} data={data} size={size} />;
